791006669b7ec0b7e720bd738159be5041a769cd
anush-web <choudhryanushka3@gmail.com>
2024-06-15 19:41:44 +0530

    trail-1

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-<html>
-  <body>
-hello    
-  </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Chat App</title>
+</head>
+<body>
+  
+
+</body>
 </html>
